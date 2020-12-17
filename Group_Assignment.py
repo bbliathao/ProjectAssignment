64f81b906493d8a29d02fe250de8950aca76f437
BLIA THAO <blia451054@mail.fresnostate.edu>
2020-12-17 02:12:06 -0800

    first commit

---
+++ Group_Assignment.py
@@ -0,0 +1,31 @@
+"""
+This program is to return the correct zodiac signs for 
+the given birth month and day.
+First we define what the zodiac signs day and month is. 
+we check to see what month it falls in, and determine the 
+day within the day that it cuts off.
+if the month is "december", then the astro sign is Sagittarius.
+    if the day is < 22 then the result will be Capricorn.
+if the month is "January", then the astro sign is Capricorn.
+    if the day is < 20 then the result will be Aquarius.
+if the month is "Febuary", then the astro sign is Aquarius.
+    if the day is < 19 then the result will be Pisces.
+if the month is "March", then the astro sign is Pisces.
+    if the day is < 21 then the result will be Aries.
+if the month is "April", then the astro sign is Aries.
+    if the day is < 20 then the result will be Taurus.
+if the month is "May", then the astro sign is Taurus.
+    if the day is < 21 then the result will be Gemini.
+if the month is "June", then the astro sign is Gemini.
+    if the day is < 22 then the result will be Cancer.
+if the month is "July", then the astro sign is Cancer.
+    if the day is < 22 then the result will be Leo.
+if the month is "August", then the astro sign is Leo.
+    if the day is < 22 then the result will be Virgo.
+if the month is "September", then the astro sign is Virgo.
+    if the day is < 22 then the result will be Libra.
+if the month is "October", then the astro sign is Libra.
+    if the day is < 22 then the result will be Scorpio.
+if the month is "November", then the astro sign is Scorpio.
+    if the day is <22 then the result will be sagittarius.
+"""
